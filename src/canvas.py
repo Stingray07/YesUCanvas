a1bf606ee5bc0cc5252be9b37099f37527c540a8
Stingray <stingrayyasuo@gmail.com>
2023-10-15 12:09:12 +0800

    New functions and convention fixes

--- src/canvas.py
+++ src/canvas.py
@@ -10,6 +10,9 @@ ANNOUNCEMENTS_URL = 'https://uc-bcf.instructure.com/api/v1/announcements'
 CALENDAR_URL = 'https://uc-bcf.instructure.com/api/v1/calendar_events'
 ACTIVE_ENROLLMENT_STATE = 'active'
 API_KEY = os.getenv("API_KEY")
+HEADERS = {
+    "Authorization": f"Bearer {API_KEY}"
+}
 
 
 @handle_req_errors
@@ -19,34 +22,42 @@ def get_current_courses_name(page_number, per_page):
         'page': page_number,
         'per_page': per_page
     }
-    headers = {
-        "Authorization": f"Bearer {API_KEY}"
-    }
+
     courses = {}
-    response = requests.get(COURSES_URL, headers=headers, params=params)
+    response = requests.get(COURSES_URL, headers=HEADERS, params=params)
     response.raise_for_status()
     data = response.json()
 
     for item in data:
+        # format_data(item)
         course_name = item.get('name')
         course_id = item.get('id')
+        original_name = item.get('original_name')
+
         if course_name.isupper():
-            courses[course_name] = course_id
+            # gets real shortened course code
+            orig_course_code = item.get('course_code')
+            course_code = get_course_code(orig_course_code)
+
+            courses[course_code] = {
+                'course_name': course_name,
+                'course_id': course_id,
+                'original_name': original_name,
+            }
 
     return courses
 
 
 @handle_req_errors
 def get_latest_announcement(course):
+    context_code = f'course_{course["course_id"]}'
+    print(context_code)
     params = {
-        'context_codes[]': [course],
+        'context_codes[]': [context_code],
         'latest_only': True
     }
-    headers = {
-        "Authorization": f"Bearer {API_KEY}"
-    }
 
-    response = requests.get(ANNOUNCEMENTS_URL, headers=headers, params=params)
+    response = requests.get(ANNOUNCEMENTS_URL, headers=HEADERS, params=params)
     response.raise_for_status()
     data = response.json()
     html_message = data[0]['message']
@@ -55,27 +66,69 @@ def get_latest_announcement(course):
 
 
 @handle_req_errors
-def get_pending_assignments(course_id):
-    assignment_url = f'{COURSES_URL}/{course_id}/assignments'
-    pending_assignments = []
+def get_pending_assignments(course):
+    course_id = course['course_id']
+    ASSIGNMENT_URL = f'{COURSES_URL}/{course_id}/assignments'
+    pending_assignments = {}
+
     params = {
         'include[]': ['submission']
     }
-    headers = {
-        "Authorization": f"Bearer {API_KEY}"
-    }
 
-    response = requests.get(assignment_url, headers=headers, params=params)
+    response = requests.get(ASSIGNMENT_URL, headers=HEADERS, params=params)
     response.raise_for_status()
     data = response.json()
 
     for assignment in data:
+        assignment_due = assignment['due_at']
+        assignment_description = assignment['description']
+        assignment_points = assignment['points_possible']
         assignment_name = assignment['name']
+        assignment_id = assignment['id']
         assignment_status = assignment['submission']['submitted_at']
         if not assignment_status:
-            pending_assignments.append(assignment_name)
+            pending_assignments[assignment_id] = {
+                'assignment_name': assignment_name,
+                'assignment_points': assignment_points,
+                'assignment_description': assignment_description,
+                'assignment_due': assignment_due,
+                'assignment_subject': course['course_name']
+            }
 
     return pending_assignments
+
+
+@handle_req_errors
+def get_teacher(course):
+    course_id = course['course_id']
+    USER_URL = f'{COURSES_URL}/{course_id}/users'
+
+    params = {
+        'enrollment_type[]': ['teacher']
+    }
+
+    response = requests.get(USER_URL, headers=HEADERS, params=params)
+    response.raise_for_status()
+    data = response.json()
+    teacher = data[0]['name']
+
+    return teacher
+
+
+@handle_req_errors
+def get_module(course):
+    course_id = course['course_id']
+    MODULES_URL = f"{COURSES_URL}/{course_id}/modules"
+
+    params = {
+        'include[]': ['items']
+    }
+
+    response = requests.get(MODULES_URL, headers=HEADERS, params=params)
+    response.raise_for_status()
+    data = response.json()
+
+    format_data(data)
 
 
 def get_all_pending_assignments(courses):
@@ -86,26 +139,11 @@ def get_all_pending_assignments(courses):
     return assignments
 
 
-@handle_req_errors
-def get_teacher(course_id):
-    user_url = f'{COURSES_URL}/{course_id}/users'
-
-    params = {
-        'enrollment_type[]': ['teacher']
-    }
-    headers = {
-        "Authorization": f"Bearer {API_KEY}"
-    }
-
-    response = requests.get(user_url, headers=headers, params=params)
-    response.raise_for_status()
-    data = response.json()
-
-    return data[0]['name']
-
-
 def format_data(data):
     print(json.dumps(data, indent=4))
 
+
 def get_course_code(orig_course_code):
-    pass
+    separator_index = orig_course_code.index("|")
+    orig_course_code = orig_course_code[:separator_index-1]
+    return orig_course_code
--- src/main.py
+++ src/main.py
@@ -5,7 +5,5 @@ PAGINATION_PER_PAGE = 20
 
 
 if __name__ == "__main__":
-    print("Hello")
     courses = cs.get_current_courses_name(per_page=PAGINATION_PER_PAGE, page_number=PAGINATION_PAGE_NUMBER)
-    cs.format_data(cs.get_teacher(courses['DESIGN AND ANALYSIS OF ALGORITHM']))
-
+    cs.get_module(courses["PE 4N"])
